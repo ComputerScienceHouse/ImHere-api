3755f98702691024ae1998e1d98a2008d0eb9892
Nathaniel Aquino <naquino14@outlook.com>
2023-11-01 21:56:29 -0400

    Create project, Hello World!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+/// Here are the routes that will be used
+/// POST /api/attendence create attendence using the json body containing the array of users, and the event info
+///     Accessed by the event hoster
+/// GET  /api/rtc gets a signalR websocket to a new attendence session 
+///     Accessed by the event hoster
+/// Viewing, adding, removing users from the attendence list is done through the signalR websocket

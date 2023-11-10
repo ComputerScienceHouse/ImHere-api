ab42cd516786adf76ab1deda8f0aa03ed53ae2a5
Nathaniel Aquino <naquino14@outlook.com>
2023-11-10 10:37:52 -0500

    Clear project

--- InMemory/AttendanceContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using ImHere_api.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace ImHere_api.InMemory;
-public class AttendanceContext : DbContext {
-    public AttendanceContext(DbContextOptions<AttendanceContext> options) : base(options) { }
-
-    public DbSet<Attendance> Attendances { get; set; } = null!;
-}
